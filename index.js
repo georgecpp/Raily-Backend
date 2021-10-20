47d50b006c101e5e4af3489d2c6182d54e42a432
androcpp <georgeandronache.cpp@gmail.com>
2021-10-20 19:02:29 +0300

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 
@@ -21,6 +22,7 @@ mongoose.connect(process.env.DB_CONNECT, () => {
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
 
 // Route Middlewares
